e980b32d8c439b95405bfd3652fb475a6e05064e
Vietdq-IOS <vietdq@infinitytech.global>
2025-10-03 21:51:33 +0700

    Update

---
+++ GoEmulator/GoEmulator/GoEmulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoEmulatorApp.swift
+//  GoEmulator
+//
+//  Created by Đỗ Việt on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GoEmulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
